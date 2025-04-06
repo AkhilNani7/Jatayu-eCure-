02657bce177fb31011a6308d0e265e1ec2201c47
AkhilNani7 <161862467+AkhilNani7@users.noreply.github.com>
2025-04-06 10:46:11 +0530

    Update models.py

--- models.py
+++ models.py
@@ -4,7 +4,6 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(UserMixin, db.Model):
-    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
